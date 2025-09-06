2f1dbaf3bb62d32bd96f784570f612a1d03fc140
Mike Monaco <mamonaco1973@gmail.com>
2025-09-06 18:05:56 -0400

    tweak validate.sh

--- validate.sh
+++ validate.sh
@@ -28,7 +28,7 @@ windows_dns=$(aws ec2 describe-instances \
 if [ -z "$windows_dns" ]; then
   echo "WARN: No Windows AD instance found with tag Name=windows-ad-admin"
 else
-  echo "NOTE: Windows Instance FQDN: $(echo $windows_dns | xargs)"
+    echo "NOTE: Windows Instance FQDN:       $(echo $windows_dns | xargs)"
 fi
 
 # --------------------------------------------------------------------------------------------------
@@ -40,7 +40,7 @@ linux_dns=$(aws ec2 describe-instances \
   --output text)
 
 if [ -z "$linux_dns" ]; then
-  echo "WARN: No EFS client instance found with tag Name=efs-samba-gateway"
+  echo "WARN: No EFS Gateway instance found with tag Name=efs-samba-gateway"
 else
-  echo "NOTE: EFS Client Instance FQDN:   $(echo $linux_dns | xargs)"
+  echo "NOTE: EFS Gateway Instance FQDN:   $(echo $linux_dns | xargs)"
 fi
