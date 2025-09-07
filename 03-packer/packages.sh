7992b8e961f940410e75462e0d44dab8f54ae179
Mike Monaco <mamonaco1973@gmail.com>
2025-09-07 17:34:22 -0400

    fix db dependencies

--- 03-packer/packages.sh
+++ 03-packer/packages.sh
@@ -37,7 +37,7 @@ apt-get install -y libcairo2-dev libxt-dev libx11-dev libxpm-dev \
 
 echo "=== Phase 5: Database & spatial libraries ==="  
 apt-get install -y libsqlite3-dev libpq-dev libmariadb-dev \
-    libudunits2-dev libgeos-dev libproj-dev libgdal-dev  
+    libmariadb-dev-compat libudunits2-dev libgeos-dev libproj-dev libgdal-dev  
 
 echo "=== Phase 6: Extra formats and science libs ==="  
 apt-get install -y libmagick++-dev libpoppler-cpp-dev \
