fcc65415c439564b4fe39134d7c35b044857f359
Mike Monaco <mamonaco1973@gmail.com>
2025-09-07 17:24:54 -0400

    configure amazon-ssm-agent

--- 03-packer/ssm.sh
+++ 03-packer/ssm.sh
@@ -14,3 +14,5 @@ sleep 30
 snap install amazon-ssm-agent --classic
 systemctl enable snap.amazon-ssm-agent.amazon-ssm-agent.service
 systemctl start snap.amazon-ssm-agent.amazon-ssm-agent.service
+  
+  
