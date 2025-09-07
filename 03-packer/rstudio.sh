515594be4a56b36e7de97842fd54799e6c414ddb
Mike Monaco <mamonaco1973@gmail.com>
2025-09-07 18:08:49 -0400

    add reticulate

--- 03-packer/rstudio.sh
+++ 03-packer/rstudio.sh
@@ -9,7 +9,7 @@ export DEBIAN_FRONTEND=noninteractive
 apt-get update
 apt-get install -y software-properties-common dirmngr
 apt-get update
-apt-get install -y r-base r-base-dev
+apt-get install -y r-base r-base-dev jsonlite png reticulate
 
 # ---------------------------------------------------------------------------------
 # Install RStudio Community Edition
