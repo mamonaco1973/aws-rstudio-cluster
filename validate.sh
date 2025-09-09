a81db6f4bc0953b156294d0b21d28eb76866c49c
Mike Monaco <mamonaco1973@gmail.com>
2025-09-09 13:28:46 -0400

    tweak validate.sh

--- validate.sh
+++ validate.sh
@@ -3,13 +3,14 @@
 # Description:
 # This script queries AWS EC2 for instances tagged with specific names and outputs their
 # associated public DNS names. It is primarily used to quickly locate endpoints for 
-# Windows and Linux AD instances deployed in AWS.
+# Windows and Linux AD instances deployed in AWS. It also retrieves the ALB DNS name.
 #
 # REQUIREMENTS:
 #   - AWS CLI installed and configured with credentials/permissions.
 #   - Instances must be tagged with:
 #       * Name = windows-ad-instance
 #       * Name = efs-client-instance
+#   - ALB must be deployed with name "rstudio-alb"
 # --------------------------------------------------------------------------------------------------
 
 # --------------------------------------------------------------------------------------------------
@@ -28,7 +29,7 @@ windows_dns=$(aws ec2 describe-instances \
 if [ -z "$windows_dns" ]; then
   echo "WARNING: No Windows AD instance found with tag Name=windows-ad-admin"
 else
-    echo "NOTE: Windows Instance FQDN:       $(echo $windows_dns | xargs)"
+  echo "NOTE: Windows Instance FQDN:       $(echo $windows_dns | xargs)"
 fi
 
 # --------------------------------------------------------------------------------------------------
@@ -43,4 +44,18 @@ if [ -z "$linux_dns" ]; then
   echo "WARNING: No EFS Gateway instance found with tag Name=efs-samba-gateway"
 else
   echo "NOTE: EFS Gateway Instance FQDN:   $(echo $linux_dns | xargs)"
+fi
+
+# --------------------------------------------------------------------------------------------------
+# Lookup ALB DNS Name
+# --------------------------------------------------------------------------------------------------
+alb_dns=$(aws elbv2 describe-load-balancers \
+  --names rstudio-alb \
+  --query 'LoadBalancers[0].DNSName' \
+  --output text)
+
+if [ -z "$alb_dns" ]; then
+  echo "WARNING: No ALB found with name rstudio-alb"
+else
+  echo "NOTE: RStudio ALB Endpoint:        http://$alb_dns"
 fi
