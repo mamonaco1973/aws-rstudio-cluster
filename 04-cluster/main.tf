3b6f40022fdee4899bdc1f2f63fb5a500db315b5
Mike Monaco <mamonaco1973@gmail.com>
2025-09-09 11:59:51 -0400

    fix second public subnet

--- 04-cluster/main.tf
+++ 04-cluster/main.tf
@@ -56,6 +56,13 @@ data "aws_subnet" "pub_subnet_1" {
   }
 }
 
+data "aws_subnet" "pub_subnet_2" {
+  filter {
+    name   = "tag:Name"
+    values = ["pub-subnet-2"]
+  }
+}
+
 data "aws_subnet" "ad_subnet" {
   filter {
     name   = "tag:Name"
